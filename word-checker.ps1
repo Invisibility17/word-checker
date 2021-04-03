fd742ae903e5aab2af1bd3188814ff53367cfa57
Rachel Eaton <=>
2021-04-03 16:30:16 -0500

    adds blank text/wordlist files and checker script with initial file selection logic

---
+++ word-checker.ps1
@@ -0,0 +1,16 @@
+﻿# Set-ExecutionPolicy -Scope CurrentUser  Unrestricted 
+
+Function Pick-Files {
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+        InitialDirectory = [Environment]::GetFolderPath('Mydocuments') 
+        MultiSelect = $true;
+    }
+    $null = $OpenFileDialog.ShowDialog();
+    return $OpenFileDialog
+}
+
+$files = Pick-Files
+foreach  ($file in $files.FileNames) {
+    $contents = Get-Content -Path $file
+}
+
